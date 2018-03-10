df33dfa8459a26f11b7ad0c327ab0f0e2dc8b287
Shubham-tiwari123 <st540362@gmail.com>
2018-03-10 23:16:02 +0530

    main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 #include "reverselevelorder.h"
+#include "treetraversal.h"
 int main(){
     struct node *root  = newNode(30);
     root->left         = newNode(20);
@@ -8,5 +9,15 @@ int main(){
     root->left->left   = newNode(41);
     root->left->right  = newNode(50); 
     
+    cout<<"\nPrinting postorder traversal:-\n";
+    postorder(root);
+    
+    cout<<"\nPrinting inorder traversal:-\n";
+    inorder(root);
+    
+    cout<<"\nPrinting preorder traversal:-\n";
+    preorder(root);
+    
+    cout<<"\nPrinting reverse level order traversal:-\n";
     reverseLevelorder(root);
 }
